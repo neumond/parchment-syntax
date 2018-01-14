ebe9f859653273c82c3ae7a641411115ab4ba27f
neumond <knifeslaughter@gmail.com>
2018-01-14 07:45:48 +0300

    Updates for js/css

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # parchment-syntax theme
 
-Little warning:
-- Early stage of development
-- Adapted mostly for python (MagicPython)
+Theme is still WIP.
+
+Adapted for:
+- Python (MagicPython)
+- Lua
+- Javascript
+- Markdown
+- HTML
+- CSS/Less
+- Json
+- Yaml
 
 ![Screenshot here](https://cloud.githubusercontent.com/assets/3260024/26567763/64657a82-4506-11e7-9f0f-13ebaa2488ee.png)
 
 ## TODO
 
 - Thin red must be darker than bold
+- .less :not operator
--- snippets/s.js
+++ snippets/s.js
@@ -1,4 +1,6 @@
-function main(a, b, c) {
+function main(a, b, c, ...args) {
+    'use strict';
+
     var x = {};
     for (var i in x) {
         console.log(c);
@@ -10,8 +12,88 @@ function main(a, b, c) {
     delete y;
 
     {
-        let h = 9;
+        let h = Number(9);
+        const k = Symbol("s");
+        const l = Boolean();
+        const s = String();
+        const a = Array();
+        const o = Object();
+        h += 5 % 3;
+        h = null;
+        h = undefined;
+        h = 3 & 5 | ^2;
+        h = false && true || (k ? "j" : "k");
     }
 
+    // comments
+
+    let fibFunc = (function fib(n){
+        return fib(n - 1) + n;
+    });
+
+    console.log(fibFunc.call(null, 3));
+    fibFunc.apply(null, [3]);
+    fibFunc.customMethod();
+    fibFunc(...args);
+
+    for (let v of arguments) {
+        let a = new Array();
+        let b = new Object();
+        let c = new String();
+    }
+
+    do {
+        console.log("x");
+    } while (i < 5)
+
+    while (false) {
+        console.error("y");
+    }
+
+    alert(window.location.href);
+
+    switch (i) {
+        case 1:
+        case 2:
+            console.info(1);
+            break;
+        default:
+            console.debug(2);
+    }
+
+    with (someThing) {
+        x = 3;
+    }
+
+    eval('alert(2)');
+
     return a + b + "string";
+}
+
+
+function Person(){
+    this.name = "John";
+    this.age = 22;
+}
+
+
+class Polygon {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+
+class Square extends Polygon {
+  constructor(sideLength) {
+    super(sideLength, sideLength);
+  }
+  get area() {
+    return this.height * this.width * math.sin(3);
+  }
+  set sideLength(newLength) {
+    this.height = newLength;
+    this.width = newLength;
+  }
 }
